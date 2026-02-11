644072fb587c886a6631622941a90183ebea472b
Hind El Hannaoui <elhannaouihind4@gmail.com>
2026-02-12 00:14:14 +0100

    java

---
+++ lab-variables.js
@@ -0,0 +1,16 @@
+var petDog = "Rex";
+var monChat = "Mimi";
+var monAge = 18;
+var catSound = "ronron";
+var dogSound = "ouaf";
+catSound = "miaou";
+
+console.log(petDog);
+console.log(monChat);
+console.log(monAge);
+console.log("Bonjour JavaScript !");
+console.log("Le nom de mon chien est : " + petDog);
+console.log("Le nom de mon chat est : " + monChat);
+console.log(petDog, "dit", dogSound);
+console.log(monChat, "dit", catSound);
+console.log(monChat, "dit maintenant", catSound);
